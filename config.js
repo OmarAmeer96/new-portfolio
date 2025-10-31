bbb2f4d842dfdb7761e791d884c90a3517f94c27
OmarAmeer96 <omar.ameer244@gmail.com>
2025-10-31 23:15:02 +0200

    Add config.js to .gitignore and create the file for Web3Forms configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+// ========== WEB3FORMS CONFIGURATION ==========
+// Keep this file secure and add it to .gitignore
+// DO NOT commit this file to GitHub
+
+const WEB3FORMS_KEY = '929ab5ae-9b13-4a35-9082-033972211f73';
